e9fcadb88b71a2fe905de2c02940a9e0a6367a05
FernandaRubio74 <20235795@esen.edu.sv>
2025-10-18 08:28:53 -0600

    user, auth & calls modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CallsService } from './calls/calls.service';
+import { CallsModule } from './calls/calls.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, CallsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CallsService],
 })
 export class AppModule {}
